5bd2d50c819227f7ba0181bd1e270acee26fa80b
C1PH3R502 <tech24into07@gmail.com>
2023-11-08 15:20:29 +0530

    project complete

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,28 +1,29 @@
-import { Card } from '@/app/ui/dashboard/cards';
+// import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 // import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+// import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton ,LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton ,LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import CardWrapper from '@/app/ui/dashboard/cards';
 
  
 export default async function Page() {
   // const revenue = await fetchRevenue();
   // const latestInvoices = await fetchLatestInvoices();
 
-  const cardData = await fetchCardData();
-  const totalPaidInvoices = cardData.totalPaidInvoices;
+  // const cardData = await fetchCardData();
+  // const totalPaidInvoices = cardData.totalPaidInvoices;
 
-  const totalPendingInvoices = cardData.totalPendingInvoices;
+  // const totalPendingInvoices = cardData.totalPendingInvoices;
 
-  const numberOfInvoices = cardData.numberOfInvoices;
+  // const numberOfInvoices = cardData.numberOfInvoices;
 
   
-  const numberOfCustomers = cardData.numberOfCustomers;
+  // const numberOfCustomers = cardData.numberOfCustomers;
 
-  console.log(numberOfCustomers)
+  // console.log(numberOfCustomers)
   
   return (
     <main>
@@ -30,14 +31,17 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
         <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        />
+        /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
       <Suspense fallback={<RevenueChartSkeleton />}>
